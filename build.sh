fa5548ed1d1c5c405588a255b596cce3d1d4919c
Chiefkat05 <jamesville05@gmail.com>
2026-02-13 00:28:11 -0600

    007 - changes to binaries

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 echo 'building for DEBUG'
 set -e
-gcc -DDEBUG -Wall -Wno-unused-function -Werror shoot_main.c -o shoot -lglfw -lGLEW -lGL -lportaudio
+gcc -DDEBUG -Wall -Wno-unused-function -Werror shoot_main.c -o shoot `pkg-config --static --libs glfw3 glew`
 ./shoot
